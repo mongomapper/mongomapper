6594c6c0cb1facc157fc890af5505aefd6aadbe9
John Nunemaker <nunemaker@gmail.com>
2009-05-23 20:43:00 -0400

    Added MongoMapper#connection and #connection= to allow reading and writing the connection.

--- lib/mongo_mapper.rb
+++ lib/mongo_mapper.rb
@@ -6,9 +6,19 @@ require 'forwardable'
 require 'rubygems'
 gem 'activesupport'
 require 'activesupport'
+gem 'mongodb-mongo'
+require 'mongo'
 
 dir = Pathname(__FILE__).dirname.expand_path + 'mongo_mapper'
 require dir + 'document'
 require dir + 'key'
 
-module MongoMapper; end
+module MongoMapper  
+  def self.connection
+    @@connection ||= XGen::Mongo::Driver::Mongo.new
+  end
+  
+  def self.connection=(new_connection)
+    @@connection = new_connection
+  end
+end
--- test/test_mongo_mapper.rb
+++ test/test_mongo_mapper.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
-# class MongoMapperTest < Test::Unit::TestCase
-#   should "probably rename this file and start testing for real" do
-#     flunk "hey buddy, you should probably rename this file and start testing for real"
-#   end
-# end
+class MongoMapperTest < Test::Unit::TestCase
+  include XGen::Mongo::Driver
+  
+  should "have connection" do
+    MongoMapper.connection.should be_instance_of(Mongo)
+  end
+  
+  should "be able to set connection information" do
+    conn = Mongo.new('localhost', 3000)
+    MongoMapper.connection = conn
+    MongoMapper.connection.should == conn
+  end
+end
