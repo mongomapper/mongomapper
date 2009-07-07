23bf282ab7a52e695f98028db1af22faeee28dfe
David A. Cuadrado <krawek@gmail.com>
2009-07-07 02:40:35 -0500

    add has_many to RailsCompatibility
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- lib/mongomapper/rails_compatibility.rb
+++ lib/mongomapper/rails_compatibility.rb
@@ -5,14 +5,17 @@ module MongoMapper
         alias_method :new_record?, :new?
         extend ClassMethods
       end
+      class << model
+        alias_method :has_many, :many
+      end
     end
-    
+
     module ClassMethods
       def column_names
         keys.keys
       end
     end
-    
+
     def to_param
       id
     end
