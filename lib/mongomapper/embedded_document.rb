af1ce905cd490b752db487c94815f14efbd64571
David A. Cuadrado <krawek@gmail.com>
2009-07-30 22:57:00 -0500

    implement find_by methods
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- lib/mongomapper/embedded_document.rb
+++ lib/mongomapper/embedded_document.rb
@@ -1,15 +1,15 @@
 require 'observer'
 
 module MongoMapper
-  module EmbeddedDocument    
+  module EmbeddedDocument
     def self.included(model)
       model.class_eval do
         extend ClassMethods
         include InstanceMethods
-        
+
         extend Associations::ClassMethods
         include Associations::InstanceMethods
-        
+
         include RailsCompatibility::EmbeddedDocument
         include Validatable
         include Serialization
@@ -17,7 +17,7 @@ module MongoMapper
         key :_id, String
       end
     end
-    
+
     module ClassMethods
       def inherited(subclass)
         unless subclass.embeddable?
@@ -26,11 +26,11 @@ module MongoMapper
         
         (@subclasses ||= []) << subclass
       end
-      
+
       def subclasses
         @subclasses
       end
-      
+
       def keys
         @keys ||= if parent = parent_model
           parent.keys.dup
@@ -38,90 +38,102 @@ module MongoMapper
           HashWithIndifferentAccess.new
         end
       end
-      
-      def key(name, type, options={})        
+
+      def key(name, type, options={})
         key = Key.new(name, type, options)
         keys[key.name] = key
-        
+
         create_accessors_for(key)
+        create_find_by_methods(key)
         add_to_subclasses(name, type, options)
         apply_validations_for(key)
         create_indexes_for(key)
-        
+
         key
       end
-      
+
       def create_accessors_for(key)
         define_method(key.name) do
           read_attribute(key.name)
         end
-        
+
         define_method("#{key.name}_before_typecast") do
           read_attribute_before_typecast(key.name)
         end
-        
+
         define_method("#{key.name}=") do |value|
           write_attribute(key.name, value)
         end
-        
+
         define_method("#{key.name}?") do
           read_attribute(key.name).present?
         end
       end
-      
+
+      def create_find_by_methods(key)
+        return if embeddable? || key.name.to_s == "_id"
+
+        class << self; self; end.instance_eval do
+          define_method("find_by_#{key.name}") do |*args|
+            options = args.extract_options!
+            find(:first, options.merge({:conditions => {key.name => args.first}}))
+          end
+        end
+      end
+
       def add_to_subclasses(name, type, options)
         return if subclasses.blank?
-        
+
         subclasses.each do |subclass|
           subclass.key name, type, options
         end
       end
-      
+
       def ensure_index(name_or_array, options={})
         keys_to_index = if name_or_array.is_a?(Array)
           name_or_array.map { |pair| [pair[0], pair[1]] }
         else
           name_or_array
         end
-        
+
         collection.create_index(keys_to_index, options.delete(:unique))
       end
-      
+
       def embeddable?
         !self.ancestors.include?(Document)
       end
-      
+
       def parent_model
         if parent = ancestors[1]
           parent if parent.ancestors.include?(EmbeddedDocument)
         end
       end
-      
+
     private
       def create_indexes_for(key)
         ensure_index key.name if key.options[:index]
       end
-      
+
       def apply_validations_for(key)
         attribute = key.name.to_sym
-        
+
         if key.options[:required]
           validates_presence_of(attribute)
         end
-        
+
         if key.options[:unique]
           validates_uniqueness_of(attribute)
         end
-        
+
         if key.options[:numeric]
           number_options = key.type == Integer ? {:only_integer => true} : {}
           validates_numericality_of(attribute, number_options)
         end
-        
+
         if key.options[:format]
           validates_format_of(attribute, :with => key.options[:format])
         end
-        
+
         if key.options[:length]
           length_options = case key.options[:length]
           when Integer
@@ -135,7 +147,7 @@ module MongoMapper
         end
       end
     end
-    
+
     module InstanceMethods
       def initialize(attrs={})
         unless attrs.nil?
@@ -144,7 +156,7 @@ module MongoMapper
               send("#{association.name}=", collection)
             end
           end
-          
+
           self.attributes = attrs
         end
         
@@ -152,33 +164,33 @@ module MongoMapper
           write_attribute :_id, XGen::Mongo::Driver::ObjectID.new.to_s
         end
       end
-      
+
       def attributes=(attrs)
         return if attrs.blank?
         attrs.each_pair do |method, value|
           self.send("#{method}=", value)
         end
       end
-      
+
       def attributes
         returning HashWithIndifferentAccess.new do |attributes|
           self.class.keys.each_pair do |name, key|
             value = value_for_key(key)
             attributes[name] = value unless value.nil?
           end
-          
+
           attributes.merge!(embedded_association_attributes)
         end
       end
-      
+
       def [](name)
         read_attribute(name)
       end
-      
+
       def []=(name, value)
         write_attribute(name, value)
       end
-      
+
       def ==(other)
         other.is_a?(self.class) && id == other.id
       end
@@ -195,7 +207,7 @@ module MongoMapper
       def using_custom_id?
         !!@using_custom_id
       end
-      
+
       def inspect
         attributes_as_nice_string = self.class.keys.keys.collect do |name|
           "#{name}: #{read_attribute(name)}"
@@ -203,7 +215,7 @@ module MongoMapper
         "#<#{self.class} #{attributes_as_nice_string}>"
       end
       
-      private        
+      private
         def value_for_key(key)
           if key.native?
             read_attribute(key.name)
@@ -212,27 +224,27 @@ module MongoMapper
             embedded_document && embedded_document.attributes
           end
         end
-      
+
         def read_attribute(name)
           value = self.class.keys[name].get(instance_variable_get("@#{name}"))
           instance_variable_set "@#{name}", value
         end
-      
+
         def read_attribute_before_typecast(name)
           instance_variable_get("@#{name}_before_typecast")
         end
-      
+
         def write_attribute(name, value)
           instance_variable_set "@#{name}_before_typecast", value
           instance_variable_set "@#{name}", self.class.keys[name].set(value)
         end
-      
+
         def embedded_association_attributes
           returning HashWithIndifferentAccess.new do |attrs|
             self.class.associations.each_pair do |name, association|
               next unless association.embeddable?
               next unless documents = instance_variable_get(association.ivar)
-            
+
               attrs[name] = documents.collect { |doc| doc.attributes }
             end
           end
