c9e494b46d313b3f6672b3fb4f4a0ddd393f6054
David A. Cuadrado <krawek@gmail.com>
2009-08-07 21:13:15 -0500

    added ManyDocumentsProxy#destroy_all
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- lib/mongomapper/associations/many_documents_proxy.rb
+++ lib/mongomapper/associations/many_documents_proxy.rb
@@ -2,38 +2,38 @@ module MongoMapper
   module Associations
     class ManyDocumentsProxy < Proxy
       delegate :klass, :to => :@association
-      
+
       def find(*args)
         options = args.extract_options!
         klass.find(*args << scoped_options(options))
       end
-      
+
       def paginate(options)
         klass.paginate(scoped_options(options))
       end
-      
+
       def all(options={})
         find(:all, scoped_options(options))
       end
-      
+
       def first(options={})
         find(:first, scoped_options(options))
       end
-      
+
       def last(options={})
         find(:last, scoped_options(options))
       end
-      
+
       def count(conditions={})
         klass.count(conditions.deep_merge(scoped_conditions))
       end
-      
+
       def replace(docs)
         @target.map(&:destroy) if load_target
         docs.each { |doc| apply_scope(doc).save }
         reset
       end
-      
+
       def <<(*docs)
         ensure_owner_saved
         flatten_deeper(docs).each { |doc| apply_scope(doc).save }
@@ -41,36 +41,40 @@ module MongoMapper
       end
       alias_method :push, :<<
       alias_method :concat, :<<
-      
+
       def build(attrs={})
         doc = klass.new(attrs)
         apply_scope(doc)
         doc
       end
-      
+
       def create(attrs={})
         doc = klass.new(attrs)
         apply_scope(doc).save
         doc
       end
-      
+
+      def destroy_all
+        @target.map(&:destroy) if load_target
+      end
+
       protected
         def scoped_conditions
           {self.foreign_key => @owner.id}
         end
-        
+
         def scoped_options(options)
           options.deep_merge({:conditions => scoped_conditions})
         end
-        
+
         def find_target
           find(:all)
         end
-        
+
         def ensure_owner_saved
           @owner.save if @owner.new?
         end
-        
+
         def apply_scope(doc)
           ensure_owner_saved
           doc.send("#{self.foreign_key}=", @owner.id)
