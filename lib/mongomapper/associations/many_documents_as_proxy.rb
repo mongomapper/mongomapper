048c8981fdd2a73a681e16366597d70888d71e88
John Nunemaker <nunemaker@gmail.com>
2009-08-28 16:16:55 -0400

    Indention.

--- lib/mongomapper/associations/many_documents_as_proxy.rb
+++ lib/mongomapper/associations/many_documents_as_proxy.rb
@@ -2,26 +2,26 @@ module MongoMapper
   module Associations
     class ManyDocumentsAsProxy < ManyDocumentsProxy
       protected
-      def scoped_conditions
-        {as_type_name => @owner.class.name, as_id_name => @owner.id}
-      end
+        def scoped_conditions
+          {as_type_name => @owner.class.name, as_id_name => @owner.id}
+        end
 
-      def apply_scope(doc)
-        ensure_owner_saved
+        def apply_scope(doc)
+          ensure_owner_saved
 
-        doc.send("#{as_type_name}=", @owner.class.name)
-        doc.send("#{as_id_name}=", @owner.id)
+          doc.send("#{as_type_name}=", @owner.class.name)
+          doc.send("#{as_id_name}=", @owner.id)
 
-        doc
-      end
+          doc
+        end
 
-      def as_type_name
-        @as_type_name ||= @association.options[:as].to_s + "_type"
-      end
+        def as_type_name
+          @as_type_name ||= @association.options[:as].to_s + "_type"
+        end
 
-      def as_id_name
-        @as_id_name ||= @association.options[:as].to_s + "_id"
-      end
+        def as_id_name
+          @as_id_name ||= @association.options[:as].to_s + "_id"
+        end
     end
   end
 end
