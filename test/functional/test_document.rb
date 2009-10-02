e2b540ea0191cf737f7bb8714ab9d29427e7e323
John Nunemaker <nunemaker@gmail.com>
2009-10-02 11:50:31 -0400

    Minor: style changes.

--- test/functional/test_document.rb
+++ test/functional/test_document.rb
@@ -979,18 +979,21 @@ class DocumentTest < Test::Unit::TestCase
       pet = Pet.new :name => 'sparky'
       @person.pets << pet
       pet.save
+      
       doc = RealPerson.find(@person.id)
       doc.pets.first.should == pet
     end
 
     should "update_attributes on the embedded document" do
-      pet = Pet.new :name => 'sparky'
+      pet = Pet.new(:name => 'sparky')
       @person.pets << pet
       pet.save
-      doc = RealPerson.find @person.id
+      
+      doc = RealPerson.find(@person.id)
       pet = doc.pets.first
       pet.update_attributes :name => 'koda'
-      doc = RealPerson.find @person.id
+      
+      doc = RealPerson.find(@person.id)
       embedded = doc.pets.first
       embedded.id.should == pet.id
       embedded.name.should == 'koda'
