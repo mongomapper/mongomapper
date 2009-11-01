c13cd05f190d30c1fdf298820eb123051f9a445a
John Nunemaker <nunemaker@gmail.com>
2009-11-01 00:24:25 -0400

    Moved validates inclusion and exclusion of to validatable and incrememted validatable version.

--- Rakefile
+++ Rakefile
@@ -13,7 +13,7 @@ Jeweler::Tasks.new do |gem|
   
   gem.add_dependency('activesupport', '>= 2.3')
   gem.add_dependency('mongo', '0.16')
-  gem.add_dependency('jnunemaker-validatable', '1.8.0')
+  gem.add_dependency('jnunemaker-validatable', '1.8.1')
   
   gem.add_development_dependency('jnunemaker-matchy', '0.4.0')
   gem.add_development_dependency('shoulda', '2.10.2')
--- lib/mongo_mapper/validations.rb
+++ lib/mongo_mapper/validations.rb
@@ -4,14 +4,6 @@ module MongoMapper
       def validates_uniqueness_of(*args)
         add_validations(args, MongoMapper::Validations::ValidatesUniquenessOf)
       end
-
-      def validates_exclusion_of(*args)
-        add_validations(args, MongoMapper::Validations::ValidatesExclusionOf)
-      end
-
-      def validates_inclusion_of(*args)
-        add_validations(args, MongoMapper::Validations::ValidatesInclusionOf)
-      end
     end
     
     class ValidatesUniquenessOf < Validatable::ValidationBase
@@ -33,38 +25,6 @@ module MongoMapper
         Array(scope).inject({}) do |conditions, key|
           conditions.merge(key => instance[key])
         end
-      end
-    end
-    
-    class ValidatesExclusionOf < Validatable::ValidationBase
-      required_option :within
-      
-      def valid?(instance)
-        value = instance[attribute]
-        return true if allow_nil && value.nil?
-        return true if allow_blank && value.blank?
-        
-        !within.include?(instance[attribute])
-      end
-      
-      def message(instance)
-        super || "is reserved"
-      end
-    end
-
-    class ValidatesInclusionOf < Validatable::ValidationBase
-      required_option :within
-      
-      def valid?(instance)
-        value = instance[attribute]
-        return true if allow_nil && value.nil?
-        return true if allow_blank && value.blank?
-        
-        within.include?(value)
-      end
-      
-      def message(instance)
-        super || "is not in the list"
       end
     end
   end
