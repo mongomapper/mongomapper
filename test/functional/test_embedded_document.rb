e74befcdcf1940c1b1d731adea1f13a2b713bf8e
John Nunemaker <nunemaker@gmail.com>
2010-01-09 23:55:46 -0500

    Refactored most of test embedded functional to use dynamic classes.

--- test/functional/test_embedded_document.rb
+++ test/functional/test_embedded_document.rb
@@ -3,21 +3,29 @@ require 'models'
 
 class EmbeddedDocumentTest < Test::Unit::TestCase
   def setup
-    @document = Doc do
-      set_collection_name 'users'
+    @klass = Doc do
       key :first_name, String
       key :last_name, String
+    end
+    
+    @pet_klass = EDoc do
+      key :name, String
+    end
+    
+    @klass.many :pets, :class => @pet_klass
+    
+    @address_class = EDoc do
+      key :city, String
+      key :state, String
     end
   end
     
   context "Saving a document with an embedded document" do
     setup do
-      @document.class_eval do
-        key :foo, Address
-      end
+      @klass.key :foo, @address_class
       
-      @address = Address.new(:city => 'South Bend', :state => 'IN')
-      @doc = @document.new(:foo => @address)
+      @address = @address_class.new(:city => 'South Bend', :state => 'IN')
+      @doc = @klass.new(:foo => @address)
     end
     
     should "embed embedded document" do
@@ -42,27 +50,25 @@ class EmbeddedDocumentTest < Test::Unit::TestCase
   
   context "new?" do
     setup do
-      @document.class_eval do
-        key :foo, Address
-      end
+      @klass.key :foo, @address_class
     end
 
     should "be new until document is saved" do
-      address = Address.new(:city => 'South Bend', :state => 'IN')
-      doc = @document.new(:foo => address)
+      address = @address_class.new(:city => 'South Bend', :state => 'IN')
+      doc = @klass.new(:foo => address)
       address.new?.should == true
     end
     
     should "not be new after document is saved" do
-      address = Address.new(:city => 'South Bend', :state => 'IN')
-      doc = @document.new(:foo => address)
+      address = @address_class.new(:city => 'South Bend', :state => 'IN')
+      doc = @klass.new(:foo => address)
       doc.save
       doc.foo.new?.should == false
     end
     
     should "not be new when document is read back" do
-      address = Address.new(:city => 'South Bend', :state => 'IN')
-      doc = @document.new(:foo => address)
+      address = @address_class.new(:city => 'South Bend', :state => 'IN')
+      doc = @klass.new(:foo => address)
       doc.save
       
       doc = doc.reload
@@ -70,53 +76,47 @@ class EmbeddedDocumentTest < Test::Unit::TestCase
     end
   end
   
-  context "save" do
-    should "save the root document" do
-      person = RealPerson.create
-
-      pet = Pet.new :name => 'sparky'
-      person.pets << pet
-      pet.save
-
-      person = person.reload
-      person.pets.first.should == pet
-    end
+  should "be able to save" do
+    person = @klass.create
     
-    should "save new keys" do
-      person = RealPerson.new
-      person[:new_attribute] = 'foobar'
-      person.save
-      
-      person = person.reload
-      person.new_attribute.should == 'foobar'
-    end
+    pet = @pet_klass.new(:name => 'sparky')
+    person.pets << pet
+    pet.save
+    
+    person.reload
+    person.pets.first.should == pet
   end
   
-  context "update_attributes" do
-    should "save the root document" do
-      person = RealPerson.create
-
-      pet = Pet.new(:name => 'sparky')
-      person.pets << pet
-      pet.save
-
-      person = person.reload
-      pet = person.pets.first
-      pet.update_attributes :name => 'koda'
-
-      person = person.reload
-      person.pets.first._id.should == pet._id
-      person.pets.first.name.should == 'koda'
-    end
-  end  
-
-  context "update_attributes!" do
-    should "pass the attributes to self.attributes" do
-      person = RealPerson.create
-      attributes = { :foo => 'bar' }
-      person.expects(:attributes=).with(attributes)
-      person.expects(:save!)
-      person.update_attributes!(attributes)
-    end
-  end  
+  should "be able to dynamically add new keys and save" do
+    person = @klass.create
+    
+    pet = @pet_klass.new(:name => 'sparky', :crazy_key => 'crazy')
+    person.pets << pet
+    pet.save
+    
+    person.reload
+    person.pets.first.crazy_key.should == 'crazy'
+  end
+  
+  should "be able to update_attributes" do
+    person = @klass.create(:pets => [@pet_klass.new(:name => 'sparky')])
+    person.reload
+    pet = person.pets.first
+    
+    pet.update_attributes(:name => 'koda')
+    person.reload
+    person.pets.first._id.should == pet._id
+    person.pets.first.name.should == 'koda'
+  end
+  
+  should "be able to update_attributes!" do
+    person = @klass.create(:pets => [@pet_klass.new(:name => 'sparky')])
+    person.reload
+    pet = person.pets.first
+    
+    attributes = {:name => 'koda'}
+    pet.expects(:attributes=).with(attributes)
+    pet.expects(:save!)
+    pet.update_attributes!(attributes)
+  end
 end
