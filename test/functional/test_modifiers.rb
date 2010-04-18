343b2ce14ea2c3cc75c32a52c2882204d3d04f37
John Nunemaker <nunemaker@gmail.com>
2010-04-18 18:47:46 -0400

    Reorganized tests.

--- test/functional/test_modifiers.rb
+++ test/functional/test_modifiers.rb
@@ -3,11 +3,11 @@ require 'test_helper'
 class ModifierTest < Test::Unit::TestCase
   def setup
     @page_class = Doc do
-      key :title, String
-      key :day_count, Integer, :default => 0
-      key :week_count, Integer, :default => 0
+      key :title,       String
+      key :day_count,   Integer, :default => 0
+      key :week_count,  Integer, :default => 0
       key :month_count, Integer, :default => 0
-      key :tags, Array
+      key :tags,        Array
     end
   end
 
@@ -18,294 +18,289 @@ class ModifierTest < Test::Unit::TestCase
     page.month_count.should == month_count
   end
 
-  context "using class methods" do
-    should "be able to increment with criteria and modifier hashes" do
-      page = @page_class.create(:title => 'Home')
-      page2 = @page_class.create(:title => 'Home')
+  context "ClassMethods" do
+    context "increment" do
+      setup do
+        @page  = @page_class.create(:title => 'Home')
+        @page2 = @page_class.create(:title => 'Home')
+      end
 
-      @page_class.increment({:title => 'Home'}, {
-        :day_count => 1, :week_count => 2, :month_count => 3
-      })
+      should "work with criteria and modifier hashes" do
+        @page_class.increment({:title => 'Home'}, :day_count => 1, :week_count => 2, :month_count => 3)
 
-      assert_page_counts page, 1, 2, 3
-      assert_page_counts page2, 1, 2, 3
+        assert_page_counts @page, 1, 2, 3
+        assert_page_counts @page2, 1, 2, 3
+      end
+
+      should "work with ids and modifier hash" do
+        @page_class.increment(@page.id, @page2.id, :day_count => 1, :week_count => 2, :month_count => 3)
+
+        assert_page_counts @page, 1, 2, 3
+        assert_page_counts @page2, 1, 2, 3
+      end
+    end
+    
+    context "decrement" do
+      setup do
+        @page = @page_class.create(:title => 'Home', :day_count => 1, :week_count => 2, :month_count => 3)
+        @page2 = @page_class.create(:title => 'Home', :day_count => 1, :week_count => 2, :month_count => 3)
+      end
+
+      should "work with criteria and modifier hashes" do
+        @page_class.decrement({:title => 'Home'}, :day_count => 1, :week_count => 2, :month_count => 3)
+
+        assert_page_counts @page, 0, 0, 0
+        assert_page_counts @page2, 0, 0, 0
+      end
+
+      should "work with ids and modifier hash" do
+        @page_class.decrement(@page.id, @page2.id, :day_count => 1, :week_count => 2, :month_count => 3)
+
+        assert_page_counts @page, 0, 0, 0
+        assert_page_counts @page2, 0, 0, 0
+      end
+      
+      should "decrement with positive or negative numbers" do
+        @page_class.decrement(@page.id, @page2.id, :day_count => -1, :week_count => 2, :month_count => -3)
+
+        assert_page_counts @page, 0, 0, 0
+        assert_page_counts @page2, 0, 0, 0
+      end
     end
 
-    should "be able to increment with ids and modifier hash" do
-      page  = @page_class.create(:title => 'Home')
-      page2 = @page_class.create(:title => 'Home')
+    context "set" do
+      setup do
+        @page  = @page_class.create(:title => 'Home')
+        @page2 = @page_class.create(:title => 'Home')
+      end
 
-      @page_class.increment(page.id, page2.id, {
-        :day_count => 1, :week_count => 2, :month_count => 3
-      })
+      should "work with criteria and modifier hashes" do
+        @page_class.set({:title => 'Home'}, :title => 'Home Revised')
 
-      assert_page_counts page, 1, 2, 3
-      assert_page_counts page2, 1, 2, 3
+        @page.reload
+        @page.title.should == 'Home Revised'
+
+        @page2.reload
+        @page2.title.should == 'Home Revised'
+      end
+
+      should "work with ids and modifier hash" do
+        @page_class.set(@page.id, @page2.id, :title => 'Home Revised')
+
+        @page.reload
+        @page.title.should == 'Home Revised'
+
+        @page2.reload
+        @page2.title.should == 'Home Revised'
+      end
     end
 
-    should "be able to decrement with criteria and modifier hashes" do
-      page = @page_class.create(:title => 'Home', :day_count => 1, :week_count => 2, :month_count => 3)
-      page2 = @page_class.create(:title => 'Home', :day_count => 1, :week_count => 2, :month_count => 3)
+    context "push" do
+      setup do
+        @page  = @page_class.create(:title => 'Home')
+        @page2 = @page_class.create(:title => 'Home')
+      end
 
-      @page_class.decrement({:title => 'Home'}, {
-        :day_count => 1, :week_count => 2, :month_count => 3
-      })
+      should "work with criteria and modifier hashes" do
+        @page_class.push({:title => 'Home'}, :tags => 'foo')
 
-      assert_page_counts page, 0, 0, 0
-      assert_page_counts page2, 0, 0, 0
+        @page.reload
+        @page.tags.should == %w(foo)
+
+        @page2.reload
+        @page.tags.should == %w(foo)
+      end
+
+      should "work with ids and modifier hash" do
+        @page_class.push(@page.id, @page2.id, :tags => 'foo')
+
+        @page.reload
+        @page.tags.should == %w(foo)
+
+        @page2.reload
+        @page.tags.should == %w(foo)
+      end
     end
 
-    should "be able to decrement with ids and modifier hash" do
-      page = @page_class.create(:title => 'Home', :day_count => 1, :week_count => 2, :month_count => 3)
-      page2 = @page_class.create(:title => 'Home', :day_count => 1, :week_count => 2, :month_count => 3)
+    context "push_all" do
+      setup do
+        @page  = @page_class.create(:title => 'Home')
+        @page2 = @page_class.create(:title => 'Home')
+        @tags = %w(foo bar)
+      end
 
-      @page_class.decrement(page.id, page2.id, {
-        :day_count => 1, :week_count => 2, :month_count => 3
-      })
+      should "work with criteria and modifier hashes" do
+        @page_class.push_all({:title => 'Home'}, :tags => @tags)
 
-      assert_page_counts page, 0, 0, 0
-      assert_page_counts page2, 0, 0, 0
+        @page.reload
+        @page.tags.should == @tags
+
+        @page2.reload
+        @page.tags.should == @tags
+      end
+
+      should "work with ids and modifier hash" do
+        @page_class.push_all(@page.id, @page2.id, :tags => @tags)
+
+        @page.reload
+        @page.tags.should == @tags
+
+        @page2.reload
+        @page.tags.should == @tags
+      end
     end
 
-    should "always decrement when decrement is called whether number is positive or negative" do
-      page = @page_class.create(:title => 'Home', :day_count => 1, :week_count => 2, :month_count => 3)
-      page2 = @page_class.create(:title => 'Home', :day_count => 1, :week_count => 2, :month_count => 3)
+    context "pull" do
+      setup do
+        @page  = @page_class.create(:title => 'Home', :tags => %w(foo bar))
+        @page2 = @page_class.create(:title => 'Home', :tags => %w(foo bar))
+      end
 
-      @page_class.decrement(page.id, page2.id, {
-        :day_count => -1, :week_count => 2, :month_count => -3
-      })
+      should "work with criteria and modifier hashes" do
+        @page_class.pull({:title => 'Home'}, :tags => 'foo')
 
-      assert_page_counts page, 0, 0, 0
-      assert_page_counts page2, 0, 0, 0
+        @page.reload
+        @page.tags.should == %w(bar)
+
+        @page2.reload
+        @page.tags.should == %w(bar)
+      end
+
+      should "be able to pull with ids and modifier hash" do
+        @page_class.pull(@page.id, @page2.id, :tags => 'foo')
+
+        @page.reload
+        @page.tags.should == %w(bar)
+
+        @page2.reload
+        @page.tags.should == %w(bar)
+      end
     end
 
-    should "be able to set with criteria and modifier hashes" do
-      page  = @page_class.create(:title => 'Home')
-      page2 = @page_class.create(:title => 'Home')
+    context "pull_all" do
+      setup do
+        @page  = @page_class.create(:title => 'Home', :tags => %w(foo bar baz))
+        @page2 = @page_class.create(:title => 'Home', :tags => %w(foo bar baz))
+      end
 
-      @page_class.set({:title => 'Home'}, :title => 'Home Revised')
+      should "work with criteria and modifier hashes" do
+        @page_class.pull_all({:title => 'Home'}, :tags => %w(foo bar))
 
-      page.reload
-      page.title.should == 'Home Revised'
+        @page.reload
+        @page.tags.should == %w(baz)
 
-      page2.reload
-      page2.title.should == 'Home Revised'
+        @page2.reload
+        @page.tags.should == %w(baz)
+      end
+
+      should "work with ids and modifier hash" do
+        @page_class.pull_all(@page.id, @page2.id, :tags => %w(foo bar))
+
+        @page.reload
+        @page.tags.should == %w(baz)
+
+        @page2.reload
+        @page.tags.should == %w(baz)
+      end
     end
 
-    should "be able to set with ids and modifier hash" do
-      page  = @page_class.create(:title => 'Home')
-      page2 = @page_class.create(:title => 'Home')
+    context "add_to_set" do
+      setup do
+        @page  = @page_class.create(:title => 'Home', :tags => 'foo')
+        @page2 = @page_class.create(:title => 'Home')
+      end
 
-      @page_class.set(page.id, page2.id, :title => 'Home Revised')
+      should "be able to add to set with criteria and modifier hash" do
+        @page_class.add_to_set({:title => 'Home'}, :tags => 'foo')
 
-      page.reload
-      page.title.should == 'Home Revised'
+        @page.reload
+        @page.tags.should == %w(foo)
 
-      page2.reload
-      page2.title.should == 'Home Revised'
+        @page2.reload
+        @page.tags.should == %w(foo)
+      end
+
+      should "be able to add to set with ids and modifier hash" do
+        @page_class.add_to_set(@page.id, @page2.id, :tags => 'foo')
+
+        @page.reload
+        @page.tags.should == %w(foo)
+
+        @page2.reload
+        @page.tags.should == %w(foo)
+      end
     end
 
-    should "be able to push with criteria and modifier hashes" do
-      page  = @page_class.create(:title => 'Home')
-      page2 = @page_class.create(:title => 'Home')
+    context "push_uniq" do
+      setup do
+        @page  = @page_class.create(:title => 'Home', :tags => 'foo')
+        @page2 = @page_class.create(:title => 'Home')        
+      end
 
-      @page_class.push({:title => 'Home'}, :tags => 'foo')
+      should "be able to push uniq with criteria and modifier hash" do
+        @page_class.push_uniq({:title => 'Home'}, :tags => 'foo')
 
-      page.reload
-      page.tags.should == %w(foo)
+        @page.reload
+        @page.tags.should == %w(foo)
 
-      page2.reload
-      page.tags.should == %w(foo)
+        @page2.reload
+        @page.tags.should == %w(foo)
+      end
+
+      should "be able to push uniq with ids and modifier hash" do
+        @page_class.push_uniq(@page.id, @page2.id, :tags => 'foo')
+
+        @page.reload
+        @page.tags.should == %w(foo)
+
+        @page2.reload
+        @page.tags.should == %w(foo)
+      end
     end
 
-    should "be able to push with ids and modifier hash" do
-      page  = @page_class.create(:title => 'Home')
-      page2 = @page_class.create(:title => 'Home')
+    context "pop" do
+      setup do
+        @page  = @page_class.create(:title => 'Home', :tags => %w(foo bar))
+      end
 
-      @page_class.push(page.id, page2.id, :tags => 'foo')
+      should "be able to remove the last element the array" do
+        @page_class.pop(@page.id, :tags => 1)
+        @page.reload
+        @page.tags.should == %w(foo)
+      end
 
-      page.reload
-      page.tags.should == %w(foo)
-
-      page2.reload
-      page.tags.should == %w(foo)
-    end
-
-    should "be able to push all with criteria and modifier hashes" do
-      page  = @page_class.create(:title => 'Home')
-      page2 = @page_class.create(:title => 'Home')
-      tags = %w(foo bar)
-
-      @page_class.push_all({:title => 'Home'}, :tags => tags)
-
-      page.reload
-      page.tags.should == tags
-
-      page2.reload
-      page.tags.should == tags
-    end
-
-    should "be able to push all with ids and modifier hash" do
-      page  = @page_class.create(:title => 'Home')
-      page2 = @page_class.create(:title => 'Home')
-      tags = %w(foo bar)
-
-      @page_class.push_all(page.id, page2.id, :tags => tags)
-
-      page.reload
-      page.tags.should == tags
-
-      page2.reload
-      page.tags.should == tags
-    end
-
-    should "be able to pull with criteria and modifier hashes" do
-      page  = @page_class.create(:title => 'Home', :tags => %w(foo bar))
-      page2 = @page_class.create(:title => 'Home', :tags => %w(foo bar))
-
-      @page_class.pull({:title => 'Home'}, :tags => 'foo')
-
-      page.reload
-      page.tags.should == %w(bar)
-
-      page2.reload
-      page.tags.should == %w(bar)
-    end
-
-    should "be able to pull with ids and modifier hash" do
-      page  = @page_class.create(:title => 'Home', :tags => %w(foo bar))
-      page2 = @page_class.create(:title => 'Home', :tags => %w(foo bar))
-
-      @page_class.pull(page.id, page2.id, :tags => 'foo')
-
-      page.reload
-      page.tags.should == %w(bar)
-
-      page2.reload
-      page.tags.should == %w(bar)
-    end
-
-    should "be able to pull all with criteria and modifier hashes" do
-      page  = @page_class.create(:title => 'Home', :tags => %w(foo bar baz))
-      page2 = @page_class.create(:title => 'Home', :tags => %w(foo bar baz))
-
-      @page_class.pull_all({:title => 'Home'}, :tags => %w(foo bar))
-
-      page.reload
-      page.tags.should == %w(baz)
-
-      page2.reload
-      page.tags.should == %w(baz)
-    end
-
-    should "be able to pull all with ids and modifier hash" do
-      page  = @page_class.create(:title => 'Home', :tags => %w(foo bar baz))
-      page2 = @page_class.create(:title => 'Home', :tags => %w(foo bar baz))
-
-      @page_class.pull_all(page.id, page2.id, :tags => %w(foo bar))
-
-      page.reload
-      page.tags.should == %w(baz)
-
-      page2.reload
-      page.tags.should == %w(baz)
-    end
-
-    should "be able to add to set with criteria and modifier hash" do
-      page  = @page_class.create(:title => 'Home', :tags => 'foo')
-      page2 = @page_class.create(:title => 'Home')
-
-      @page_class.add_to_set({:title => 'Home'}, :tags => 'foo')
-
-      page.reload
-      page.tags.should == %w(foo)
-
-      page2.reload
-      page.tags.should == %w(foo)
-    end
-
-    should "be able to add to set with ids and modifier hash" do
-      page  = @page_class.create(:title => 'Home', :tags => 'foo')
-      page2 = @page_class.create(:title => 'Home')
-
-      @page_class.add_to_set(page.id, page2.id, :tags => 'foo')
-
-      page.reload
-      page.tags.should == %w(foo)
-
-      page2.reload
-      page.tags.should == %w(foo)
-    end
-
-    should "be able to push uniq with criteria and modifier hash" do
-      page  = @page_class.create(:title => 'Home', :tags => 'foo')
-      page2 = @page_class.create(:title => 'Home')
-
-      @page_class.push_uniq({:title => 'Home'}, :tags => 'foo')
-
-      page.reload
-      page.tags.should == %w(foo)
-
-      page2.reload
-      page.tags.should == %w(foo)
-    end
-
-    should "be able to push uniq with ids and modifier hash" do
-      page  = @page_class.create(:title => 'Home', :tags => 'foo')
-      page2 = @page_class.create(:title => 'Home')
-
-      @page_class.push_uniq(page.id, page2.id, :tags => 'foo')
-
-      page.reload
-      page.tags.should == %w(foo)
-
-      page2.reload
-      page.tags.should == %w(foo)
-    end
-
-    should "be able to remove the last element the array" do
-      page  = @page_class.create(:title => 'Home', :tags => %w(foo bar))
-      @page_class.pop(page.id, :tags => 1)
-      page.reload
-      page.tags.should == %w(foo)
-    end
-
-    should "be able to remove the first element of the array" do
-      page  = @page_class.create(:title => 'Home', :tags => %w(foo bar))
-      @page_class.pop(page.id, :tags => -1)
-      page.reload
-      page.tags.should == %w(bar)
+      should "be able to remove the first element of the array" do
+        @page_class.pop(@page.id, :tags => -1)
+        @page.reload
+        @page.tags.should == %w(bar)
+      end
     end
   end
 
-  context "using instance methods" do
+  context "InstanceMethods" do
     should "be able to increment with modifier hashes" do
       page = @page_class.create
-
-      page.increment({:day_count => 1, :week_count => 2, :month_count => 3})
+      page.increment(:day_count => 1, :week_count => 2, :month_count => 3)
 
       assert_page_counts page, 1, 2, 3
     end
 
     should "be able to decrement with modifier hashes" do
       page = @page_class.create(:day_count => 1, :week_count => 2, :month_count => 3)
-
-      page.decrement({:day_count => 1, :week_count => 2, :month_count => 3})
+      page.decrement(:day_count => 1, :week_count => 2, :month_count => 3)
 
       assert_page_counts page, 0, 0, 0
     end
 
     should "always decrement when decrement is called whether number is positive or negative" do
       page = @page_class.create(:day_count => 1, :week_count => 2, :month_count => 3)
-
-      page.decrement({:day_count => -1, :week_count => 2, :month_count => -3})
+      page.decrement(:day_count => -1, :week_count => 2, :month_count => -3)
 
       assert_page_counts page, 0, 0, 0
     end
 
     should "be able to set with modifier hashes" do
       page  = @page_class.create(:title => 'Home')
-
       page.set(:title => 'Home Revised')
 
       page.reload
@@ -314,7 +309,6 @@ class ModifierTest < Test::Unit::TestCase
 
     should "be able to push with modifier hashes" do
       page = @page_class.create
-
       page.push(:tags => 'foo')
 
       page.reload
@@ -322,8 +316,7 @@ class ModifierTest < Test::Unit::TestCase
     end
 
     should "be able to pull with criteria and modifier hashes" do
-      page  = @page_class.create(:tags => %w(foo bar))
-
+      page = @page_class.create(:tags => %w(foo bar))
       page.pull(:tags => 'foo')
 
       page.reload
@@ -360,8 +353,7 @@ class ModifierTest < Test::Unit::TestCase
     
     should "be able to pop with modifier hashes" do
       page = @page_class.create(:tags => %w(foo bar))
-
-      page.pop({:tags => 1})
+      page.pop(:tags => 1)
 
       page.reload
       page.tags.should == %w(foo)
