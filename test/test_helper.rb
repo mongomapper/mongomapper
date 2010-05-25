aa0efc548083ef01d18e6995f1d62d0a5bff7ea8
John Nunemaker <nunemaker@gmail.com>
2010-05-25 16:38:35 -0400

    Moved custom matchers into test helper and remove test/support folder.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,11 +15,7 @@ require 'mocha'
 require 'json'
 require 'pp'
 
-require 'support/custom_matchers'
-
 class Test::Unit::TestCase
-  include CustomMatchers
-
   def Doc(name=nil, &block)
     klass = Class.new do
       include MongoMapper::Document
@@ -54,6 +50,42 @@ class Test::Unit::TestCase
     if klass.database.collection_names.include?(klass.collection.name)
       klass.collection.drop_indexes
     end
+  end
+  
+  custom_matcher :be_true do |receiver, matcher, args|
+    matcher.positive_failure_message = "Expected #{receiver} to be true but it wasn't"
+    matcher.negative_failure_message = "Expected #{receiver} not to be true but it was"
+    receiver.eql?(true)
+  end
+
+  custom_matcher :be_false do |receiver, matcher, args|
+    matcher.positive_failure_message = "Expected #{receiver} to be false but it wasn't"
+    matcher.negative_failure_message = "Expected #{receiver} not to be false but it was"
+    receiver.eql?(false)
+  end
+
+  custom_matcher :have_error_on do |receiver, matcher, args|
+    receiver.valid?
+    attribute = args[0]
+    expected_message = args[1]
+
+    if expected_message.nil?
+      matcher.positive_failure_message = "#{receiver} had no errors on #{attribute}"
+      matcher.negative_failure_message = "#{receiver} had errors on #{attribute} #{receiver.errors.inspect}"
+      !receiver.errors.on(attribute).blank?
+    else
+      actual = receiver.errors.on(attribute)
+      matcher.positive_failure_message = %Q(Expected error on #{attribute} to be "#{expected_message}" but was "#{actual}")
+      matcher.negative_failure_message = %Q(Expected error on #{attribute} not to be "#{expected_message}" but was "#{actual}")
+      actual == expected_message
+    end
+  end
+
+  custom_matcher :have_index do |receiver, matcher, args|
+    index_name = args[0]
+    matcher.positive_failure_message = "#{receiver} does not have index named #{index_name}, but should"
+    matcher.negative_failure_message = "#{receiver} does have index named #{index_name}, but should not"
+    !receiver.collection.index_information.detect { |index| index[0] == index_name }.nil?
   end
 end
 
