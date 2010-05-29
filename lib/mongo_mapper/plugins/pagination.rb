427c8177868c3a66857b6ae915b75041e838ead4
John Nunemaker <nunemaker@gmail.com>
2010-05-29 11:15:47 -0400

    Replaced paginate with Plucky::Query#paginate.

--- lib/mongo_mapper/plugins/pagination.rb
+++ lib/mongo_mapper/plugins/pagination.rb
@@ -3,18 +3,10 @@ module MongoMapper
   module Plugins
     module Pagination
       module ClassMethods
-        def per_page
-          25
-        end
+        def per_page; 25 end
 
-        def paginate(options)
-          per_page      = options.delete(:per_page) || self.per_page
-          page          = options.delete(:page)
-          total_entries = count(options)
-          pagination    = Pagination::Proxy.new(total_entries, page, per_page)
-          options.update(:limit => pagination.limit, :skip => pagination.skip)
-          pagination.subject = find_many(options)
-          pagination
+        def paginate(opts={})
+          query.paginate({:per_page => per_page}.merge(opts))
         end
       end
     end
