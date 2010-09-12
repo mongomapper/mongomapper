1fe01b62636a6fc8657f6c221ef9b3906c3314d0
Brandon Keepers <brandon@collectiveidea.com>
2010-09-12 17:19:49 -0400

    fixes for railtie

--- lib/mongo_mapper/railtie.rb
+++ lib/mongo_mapper/railtie.rb
@@ -34,17 +34,18 @@ module MongoMapper
       end
     end
 
-    # See http://groups.google.com/group/mongomapper/browse_thread/thread/68f62e8eda43b43a/4841dba76938290c
-    #
-    # to_prepare is called before each request in development mode and the first request in production.
-    config.to_prepare do
-      unless config.cache_classes
-        # Rails reloading was making descendants fill up and leak memory, these make sure they get cleared
-        MongoMapper::Document.descendants.each {|m| m.descendants.clear if m.respond_to?(:descendants) }
-        MongoMapper::Document.descendants.clear
-        MongoMapper::EmbeddedDocument.descendants.each {|m| m.descendants.clear if m.respond_to?(:descendants) }
-        MongoMapper::EmbeddedDocument.descendants.clear
-        MongoMapper::Plugins::IdentityMap.models.clear
+    initializer "mongo_mapper.prepare_dispatcher" do |app|
+      # See http://groups.google.com/group/mongomapper/browse_thread/thread/68f62e8eda43b43a/4841dba76938290c
+      # to_prepare is called before each request in development mode and the first request in production.
+      ActionDispatch::Callbacks.to_prepare do
+        unless app.config.cache_classes
+          # Rails reloading was making descendants fill up and leak memory, these make sure they get cleared
+          MongoMapper::Document.descendants.each {|m| m.descendants.clear if m.respond_to?(:descendants) }
+          MongoMapper::Document.descendants.clear
+          MongoMapper::EmbeddedDocument.descendants.each {|m| m.descendants.clear if m.respond_to?(:descendants) }
+          MongoMapper::EmbeddedDocument.descendants.clear
+          MongoMapper::Plugins::IdentityMap.models.clear
+        end
       end
     end
   end
