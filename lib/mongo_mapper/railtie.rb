fa83505a431e86558f53ca8a40b2ed41fa9813e0
Brandon Keepers <brandon@collectiveidea.com>
2010-09-12 16:47:53 -0400

    Load config/mongo.yml if it is present

--- lib/mongo_mapper/railtie.rb
+++ lib/mongo_mapper/railtie.rb
@@ -5,13 +5,25 @@ require "active_model/railtie"
 module MongoMapper
   # = MongoMapper Railtie
   class Railtie < Rails::Railtie
+
     config.mongo_mapper = ActiveSupport::OrderedOptions.new
+
+    config.paths.config.mongo 'config/mongo.yml'
 
     initializer "mongo_mapper.set_configs" do |app|
       ActiveSupport.on_load(:mongo_mapper) do
         app.config.mongo_mapper.each do |k,v|
           send "#{k}=", v
         end
+      end
+    end
+
+    # This sets the database configuration and establishes the connection.
+    initializer "mongo_mapper.initialize_database" do
+      config_file = config.paths.config.mongo
+      if config_file.file?
+        settings = YAML.load(ERB.new(config_file.read).result)[Rails.env]
+        MongoMapper.setup(settings)
       end
     end
 
