af178b859254776b6de1c1062e133d541be820ff
Brandon Keepers <brandon@collectiveidea.com>
2010-09-23 00:16:34 -0400

    Fix conflicting variable name that was causing an infinite loop

--- lib/mongo_mapper/plugins/embedded_callbacks.rb
+++ lib/mongo_mapper/plugins/embedded_callbacks.rb
@@ -20,14 +20,14 @@ module MongoMapper
             embedded_docs += Array(get_proxy(association).send(:load_target))
           end
 
-          chain = embedded_docs.inject(block) do |block, doc|
+          block = embedded_docs.inject(block) do |chain, doc|
             if doc.class.respond_to?("_#{callback}_callbacks")
-              lambda { doc.run_callbacks(callback, &block) }
+              lambda { doc.run_callbacks(callback, &chain) }
             else
-              block
+              chain
             end
           end
-          super callback, &chain
+          super callback, &block
         end
       end
     end
