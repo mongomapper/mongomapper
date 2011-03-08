70581fa741d21a48bd739534a464db7fabcaf7b7
Brandon Keepers <brandon@opensoul.org>
2011-03-07 21:12:25 -0500

    Set logger on connection to Rails.logger
    Closes #216

--- lib/mongo_mapper/railtie.rb
+++ lib/mongo_mapper/railtie.rb
@@ -25,7 +25,7 @@ module MongoMapper
       config_file = Rails.root.join('config/mongo.yml')
       if config_file.file?
         config = YAML.load(ERB.new(config_file.read).result)
-        MongoMapper.setup(config, Rails.env)
+        MongoMapper.setup(config, Rails.env, :logger => Rails.logger)
       end
     end
 
