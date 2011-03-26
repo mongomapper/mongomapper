91a08c2db578088062aec42020a8fab3997e9765
John Nunemaker <nunemaker@gmail.com>
2011-03-26 15:39:36 -0400

    Changed test database name.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -95,5 +95,5 @@ logger = Logger.new(log_dir + '/test.log')
 
 LogBuddy.init(:logger => logger)
 MongoMapper.connection = Mongo::Connection.new('127.0.0.1', 27017, :logger => logger)
-MongoMapper.database = "mm-test-#{RUBY_VERSION.gsub('.', '-')}"
+MongoMapper.database = "test"
 MongoMapper.database.collections.each { |c| c.drop_indexes }
