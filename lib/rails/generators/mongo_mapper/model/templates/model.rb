5dc6b9a86ffab5cd1195820af86e2312170b11b5
Brandon Keepers <brandon@opensoul.org>
2011-03-28 08:47:38 -0400

    Fix attribute type in generator

--- lib/rails/generators/mongo_mapper/model/templates/model.rb
+++ lib/rails/generators/mongo_mapper/model/templates/model.rb
@@ -2,7 +2,7 @@ class <%= class_name %>
   include MongoMapper::Document
 
 <% attributes.each do |attribute| -%>
-  key :<%= attribute.name %>, <%= attribute.to_s.camelcase %>
+  key :<%= attribute.name %>, <%= attribute.type.to_s.camelcase %>
 <% end -%>
 <% if options[:timestamps] %>
   timestamps!
