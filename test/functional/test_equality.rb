b2327d5162721e78272b475f247baade4a3ed2b9
Brandon Keepers <brandon@opensoul.org>
2011-05-14 11:22:57 -0400

    Use dynamic classes in the equality tests

--- test/functional/test_equality.rb
+++ test/functional/test_equality.rb
@@ -1,24 +1,20 @@
 require 'test_helper'
 
-class Person
-  include MongoMapper::Document
-
-  one :address
-end
-
-class Address
-  include MongoMapper::Document
-
-  belongs_to :person
-end
-
 class EqualityTest < Test::Unit::TestCase
   context "Case equality" do
+    setup do
+      @person = Doc()
+      @address = Doc()
+
+      @person.one :address, :class => @address, :foreign_key => :person_id
+      @address.belongs_to :person, :class => @person
+    end
+
     should "work with proxies" do
-      person = Person.create
-      address = Address.create(:person => person)
-      Person.should === address.person
-      Address.should === person.address
+      person = @person.create!
+      address = @address.create!(:person => person)
+      @person.should === address.person
+      @address.should === person.address
     end
   end
 end
