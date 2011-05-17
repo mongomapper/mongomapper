85b7f6bc620b87c286bf345b9c3d52f555a383f8
Brandon Keepers <brandon@opensoul.org>
2011-05-17 14:25:49 -0400

    Remove support for the deprecated plugin structure

--- lib/mongo_mapper/plugins.rb
+++ lib/mongo_mapper/plugins.rb
@@ -8,14 +8,8 @@ module MongoMapper
     end
 
     def plugin(mod)
-      if ActiveSupport::Concern === mod
-        include mod
-      else
-        warn "[DEPRECATED] Plugins must extend ActiveSupport::Concern"
-        extend mod::ClassMethods     if mod.const_defined?(:ClassMethods)
-        include mod::InstanceMethods if mod.const_defined?(:InstanceMethods)
-        mod.configure(self)          if mod.respond_to?(:configure)
-      end
+      raise ArgumentError,  "Plugins must extend ActiveSupport::Concern" unless ActiveSupport::Concern === mod
+      include mod
       direct_descendants.each {|model| model.send(:include, mod) }
       plugins << mod
     end
