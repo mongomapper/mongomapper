06b36ff9e0212950c0e586228a570082449629da
Mark Coates (oddlyzen) <mark.coates@gmail.com>
2011-06-17 13:21:02 -0400

    Added to Userstamps plugin a way to use a unconventionally named User class (e.g., SpecialUser).

--- test/functional/test_userstamps.rb
+++ test/functional/test_userstamps.rb
@@ -6,22 +6,28 @@ class UserstampsTest < Test::Unit::TestCase
       @document = Doc do
         userstamps!
       end
+      @document_alt_user = Doc do
+        userstamps! 'AltUser'
+      end
+      @document_for_alt_user = Doc do
+        userstamps_for! 'AltUser'
+      end
     end
 
     should "add creator_id key" do
-      @document.keys.keys.should include('creator_id')
+      [@document, @document_alt_user, @document_for_alt_user].each{ |d| d.keys.keys.should include('creator_id') }
     end
 
     should "add updater_id key" do
-      @document.keys.keys.should include('updater_id')
+      [@document, @document_alt_user, @document_for_alt_user].each{ |d| d.keys.keys.should include('updater_id') }
     end
 
     should "add belongs_to creator" do
-      @document.associations.keys.should include(:creator)
+      [@document, @document_alt_user, @document_for_alt_user].each{ |d| d.associations.keys.should include(:creator) }
     end
 
     should "add belongs_to updater" do
-      @document.associations.keys.should include(:updater)
+      [@document, @document_alt_user, @document_for_alt_user].each{ |d| d.associations.keys.should include(:updater) }
     end
   end
 end
