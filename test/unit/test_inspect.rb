78415db4358512b0f7f8df9e05362510c6bd55d4
Jonathan Chambers <j.chambers@gmx.net>
2011-07-21 09:23:52 +0100

    added test

--- test/unit/test_inspect.rb
+++ test/unit/test_inspect.rb
@@ -6,13 +6,18 @@ class InspectTest < Test::Unit::TestCase
       @document = Doc('User') do
         key :name,  String
         key :age,   Integer
+        key :email, String
       end
 
       @doc = @document.new(:name => 'John', :age => 29)
     end
 
-    should "print out attributes in alpha sorted order" do
+    should "print out non-nil attributes in alpha sorted order" do
       @doc.inspect.should =~ /_id:.*, age: 29, name: "John"/
+    end
+    
+    should "print out all attributes when (optional) include_super argument is true" do
+      @doc.inspect(true).should =~ /_id:.*, age: 29, email: nil, name: "John"/
     end
 
     should "include class name" do
