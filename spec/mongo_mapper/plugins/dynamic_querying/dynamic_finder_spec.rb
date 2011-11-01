c2d235d09120c6afa1724b33736f67d5b72ae16f
Brandon Keepers <brandon@opensoul.org>
2011-10-31 23:51:32 -0400

    Port dynamic finder test to rspec

--- spec/mongo_mapper/plugins/dynamic_querying/dynamic_finder_spec.rb
+++ spec/mongo_mapper/plugins/dynamic_querying/dynamic_finder_spec.rb
@@ -1,35 +1,35 @@
-require 'test_helper'
+require 'spec_helper'
 
-class DynamicFinderTest < Test::Unit::TestCase
+describe MongoMapper::Plugins::DynamicQuerying::DynamicFinder do
   DynamicFinder = MongoMapper::Plugins::DynamicQuerying::DynamicFinder
 
-  should "initialize with method" do
+  it "should initialize with method" do
     finder = DynamicFinder.new(:foobar)
     finder.method.should == :foobar
   end
 
   context "found?" do
-    should "be true for find_by" do
+    it "should be true for find_by" do
       DynamicFinder.new(:find_by_foo).found?.should be_true
     end
 
-    should "be true for find_by with !" do
+    it "should be true for find_by with !" do
       DynamicFinder.new(:find_by_foo!).found?.should be_true
     end
 
-    should "be true for find_all_by" do
+    it "should be true for find_all_by" do
       DynamicFinder.new(:find_all_by_foo).found?.should be_true
     end
 
-    should "be true for find_or_initialize_by" do
+    it "should be true for find_or_initialize_by" do
       DynamicFinder.new(:find_or_initialize_by_foo).found?.should be_true
     end
 
-    should "be true for find_or_create_by" do
+    it "should be true for find_or_create_by" do
       DynamicFinder.new(:find_or_create_by_foo).found?.should be_true
     end
 
-    should "be false for anything else" do
+    it "should be false for anything else" do
       [:foobar, :bazwick].each do |method|
         DynamicFinder.new(method).found?.should be_false
       end
@@ -37,88 +37,88 @@ class DynamicFinderTest < Test::Unit::TestCase
   end
 
   context "find_all_by" do
-    should "parse one attribute" do
+    it "should parse one attribute" do
       DynamicFinder.new(:find_all_by_foo).attributes.should == %w(foo)
     end
 
-    should "parse multiple attributes" do
+    it "should parse multiple attributes" do
       DynamicFinder.new(:find_all_by_foo_and_bar).attributes.should == %w(foo bar)
       DynamicFinder.new(:find_all_by_foo_and_bar_and_baz).attributes.should == %w(foo bar baz)
     end
 
-    should "set finder to :all" do
+    it "should set finder to :all" do
       DynamicFinder.new(:find_all_by_foo_and_bar).finder.should == :all
     end
   end
 
   context "find_by" do
-    should "parse one attribute" do
+    it "should parse one attribute" do
       DynamicFinder.new(:find_by_foo).attributes.should == %w(foo)
     end
 
-    should "parse multiple attributes" do
+    it "should parse multiple attributes" do
       DynamicFinder.new(:find_by_foo_and_bar).attributes.should == %w(foo bar)
     end
 
-    should "set finder to :first" do
+    it "should set finder to :first" do
       DynamicFinder.new(:find_by_foo).finder.should == :first
     end
 
-    should "set bang to false" do
+    it "should set bang to false" do
       DynamicFinder.new(:find_by_foo).bang.should be_false
     end
   end
 
   context "find_by with !" do
-    should "parse one attribute" do
+    it "should parse one attribute" do
       DynamicFinder.new(:find_by_foo!).attributes.should == %w(foo)
     end
 
-    should "parse multiple attributes" do
+    it "should parse multiple attributes" do
       DynamicFinder.new(:find_by_foo_and_bar!).attributes.should == %w(foo bar)
     end
 
-    should "set finder to :first" do
+    it "should set finder to :first" do
       DynamicFinder.new(:find_by_foo!).finder.should == :first
     end
 
-    should "set bang to true" do
+    it "should set bang to true" do
       DynamicFinder.new(:find_by_foo!).bang.should be_true
     end
   end
 
   context "find_or_initialize_by" do
-    should "parse one attribute" do
+    it "should parse one attribute" do
       DynamicFinder.new(:find_or_initialize_by_foo).attributes.should == %w(foo)
     end
 
-    should "parse multiple attributes" do
+    it "should parse multiple attributes" do
       DynamicFinder.new(:find_or_initialize_by_foo_and_bar).attributes.should == %w(foo bar)
     end
 
-    should "set finder to :first" do
+    it "should set finder to :first" do
       DynamicFinder.new(:find_or_initialize_by_foo).finder.should == :first
     end
 
-    should "set instantiator to new" do
+    it "should set instantiator to new" do
       DynamicFinder.new(:find_or_initialize_by_foo).instantiator.should == :new
     end
   end
 
   context "find_or_create_by" do
-    should "parse one attribute" do
+    it "should parse one attribute" do
       DynamicFinder.new(:find_or_create_by_foo).attributes.should == %w(foo)
     end
 
-    should "parse multiple attributes" do
+    it "should parse multiple attributes" do
       DynamicFinder.new(:find_or_create_by_foo_and_bar).attributes.should == %w(foo bar)
     end
 
-    should "set finder to :first" do
+    it "should set finder to :first" do
       DynamicFinder.new(:find_or_create_by_foo).finder.should == :first
     end
 
-    should "set instantiator to new" do
+    it "should set instantiator to new" do
       DynamicFinder.new(:find_or_create_by_foo).instantiator.should == :create
     end
   end
