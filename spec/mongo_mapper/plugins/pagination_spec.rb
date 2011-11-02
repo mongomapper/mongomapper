ebfb3e15a0fbe2abedc87a362e3a8ced68af7eee
Brandon Keepers <brandon@opensoul.org>
2011-11-02 09:15:41 -0400

    Port pagination test to rspec

---
+++ spec/mongo_mapper/plugins/pagination_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe MongoMapper::Plugins::Pagination do
+  it "should default per_page to 25" do
+    Doc().per_page.should == 25
+  end
+
+  it "should allow overriding per_page" do
+    Doc() { def self.per_page; 1 end }.per_page.should == 1
+  end
+end
