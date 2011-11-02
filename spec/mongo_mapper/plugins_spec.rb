cab01f01f881df0866ea84377b23ada632967a4b
Brandon Keepers <brandon@opensoul.org>
2011-11-02 09:37:56 -0400

    Port plugins test to rspec

--- spec/mongo_mapper/plugins_spec.rb
+++ spec/mongo_mapper/plugins_spec.rb
@@ -1,7 +1,7 @@
-require 'test_helper'
+require 'spec_helper'
 
-class PluginsTest < Test::Unit::TestCase
-  should "default plugins to empty array" do
+describe MongoMapper::Plugins do
+  it "should default plugins to empty array" do
     Class.new { extend MongoMapper::Plugins }.plugins.should == []
   end
 
@@ -26,41 +26,41 @@ class PluginsTest < Test::Unit::TestCase
       end
     end
 
-    setup do
+    before do
       @document = Class.new do
         extend MongoMapper::Plugins
         plugin MyConcern
       end
     end
 
-    should "include instance methods" do
+    it "should include instance methods" do
       @document.new.instance_foo.should == 'instance_foo'
     end
 
-    should "extend class methods" do
+    it "should extend class methods" do
       @document.class_foo.should == 'class_foo'
     end
 
-    should "pass model to configure" do
+    it "should pass model to configure" do
       @document.new.should respond_to(:from_concern)
     end
 
-    should "add plugin to plugins" do
+    it "should add plugin to plugins" do
       @document.plugins.should include(MyConcern)
     end
 
     context "Document" do
-      setup do
+      before do
         MongoMapper::Document.plugins.delete(MyConcern)
       end
 
-      should 'allow plugins on Document' do
+      it "should allow plugins on Document" do
         MongoMapper::Document.plugin(MyConcern)
         Doc().should respond_to(:class_foo)
         Doc().new.should respond_to(:instance_foo)
       end
 
-      should 'add plugins to classes that include Document before they are added' do
+      it "should add plugins to classes that include Document before they are added" do
         article = Doc()
         MongoMapper::Document.plugin(MyConcern)
         article.should respond_to(:class_foo)
@@ -69,18 +69,18 @@ class PluginsTest < Test::Unit::TestCase
     end
 
     context "EmbeddedDocument" do
-      setup do
+      before do
         MongoMapper::EmbeddedDocument.plugins.delete(MyConcern)
       end
 
-      should 'allow plugins on EmbeddedDocument' do
+      it "should allow plugins on EmbeddedDocument" do
         MongoMapper::EmbeddedDocument.plugin(MyConcern)
         article = EDoc()
         article.should respond_to(:class_foo)
         article.new.should respond_to(:instance_foo)
       end
 
-      should 'add plugins to classes that include EmbeddedDocument before they are added' do
+      it "should add plugins to classes that include EmbeddedDocument before they are added" do
         article = EDoc()
         MongoMapper::EmbeddedDocument.plugin(MyConcern)
         article.should respond_to(:class_foo)
