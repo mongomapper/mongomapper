4631e61e411faf54cc3c8e0b5ac619c2d9a26c4d
Brandon Keepers <brandon@opensoul.org>
2011-11-02 10:02:44 -0400

    Port translation test to rspec

--- spec/mongo_mapper/translation_spec.rb
+++ spec/mongo_mapper/translation_spec.rb
@@ -1,15 +1,15 @@
-require 'test_helper'
+require 'spec_helper'
 
-class TranslationTest < Test::Unit::TestCase
-  should "translate add mongo_mapper translations" do
+describe MongoMapper::Translation do
+  it "should translate add mongo_mapper translations" do
     I18n.translate("mongo_mapper.errors.messages.taken").should == "has already been taken"
   end
 
-  should "set i18n_scope" do
+  it "should set i18n_scope" do
     Doc().i18n_scope.should == :mongo_mapper
   end
 
-  should "translate document attributes" do
+  it "should translate document attributes" do
     I18n.config.backend.store_translations(:en, :mongo_mapper => {:attributes => {:thing => {:foo => 'Bar'}}})
     doc = Doc('Thing') do
       key :foo, String
@@ -17,7 +17,7 @@ class TranslationTest < Test::Unit::TestCase
     doc.human_attribute_name(:foo).should == 'Bar'
   end
 
-  should "translate embedded document attributes" do
+  it "should translate embedded document attributes" do
     I18n.config.backend.store_translations(:en, :mongo_mapper => {:attributes => {:thing => {:foo => 'Bar'}}})
     doc = EDoc('Thing') do
       key :foo, String
