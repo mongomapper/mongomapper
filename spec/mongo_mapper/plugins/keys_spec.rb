3e23d57804ccf9d2ace2af8b2ed1e727181d6902
Brandon Keepers <brandon@opensoul.org>
2011-11-02 09:09:02 -0400

    Port keys test to rspec

--- spec/mongo_mapper/plugins/keys_spec.rb
+++ spec/mongo_mapper/plugins/keys_spec.rb
@@ -65,5 +65,64 @@ describe MongoMapper::Plugins::Keys do
 
   end
 
+  context ".new with no id and _id of type integer" do
+    it "should not error" do
+      lambda {
+        klass = Doc() do
+          key :_id, Integer
+        end
+        # No sensible default id for integer, people better pass them in if they user this
+        silence_stderr { klass.new.id.should be_nil }
+      }.should_not raise_error
+    end
+  end
 
+  context ".key?(:symbol)" do
+    it "should be true if document has key" do
+      Address.key?(:city).should be_true
+    end
+
+    it "should be false if document does not have key" do
+      Address.key?(:foo).should be_false
+    end
+  end
+
+  context ".key?('string')" do
+    it "should be true if document has key" do
+      Address.key?('city').should be_true
+    end
+
+    it "should be false if document does not have key" do
+      Address.key?('foo').should be_false
+    end
+  end
+
+  context ".new (from database)" do
+    before do
+      @klass = Doc do
+        key :user, Hash
+
+        def user=(user)
+          super(:id => user.id, :name => user.name)
+        end
+      end
+
+      user_class = Struct.new(:id, :name)
+      @klass.create(:user => user_class.new(1, 'John Nunemaker'))
+    end
+
+    it "should use []= for keys instead of public writer" do
+      lambda {
+        doc = @klass.first
+        doc.user['id'].should   == 1
+        doc.user['name'].should == 'John Nunemaker'
+      }.should_not raise_error
+    end
+  end
+
+  context ".load" do
+    it "shouldreturn nil if argument is nil" do
+      Doc().load(nil).should be_nil
+    end
+  end
 end
