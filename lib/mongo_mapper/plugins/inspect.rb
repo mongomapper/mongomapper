9aba10d620709a11922c1a2fd7f440f091ccdb4f
Kamil Kuzma <kamil.kuzma@gmail.com>
2012-02-12 03:15:43 +0100

    Fix and test for displaying embedded documents

--- lib/mongo_mapper/plugins/inspect.rb
+++ lib/mongo_mapper/plugins/inspect.rb
@@ -7,7 +7,11 @@ module MongoMapper
       def inspect(include_nil = false)
         keys = include_nil ? key_names : attributes.keys
         attributes_as_nice_string = keys.sort.collect do |name|
-          "#{name}: #{self[name].inspect}"
+          if self[name].nil?
+            "#{name}: #{self.send(:"#{name}").inspect}"
+          else
+            "#{name}: #{self[name].inspect}"
+          end
         end.join(", ")
         "#<#{self.class} #{attributes_as_nice_string}>"
       end
