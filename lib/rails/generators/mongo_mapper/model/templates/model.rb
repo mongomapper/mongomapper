fa93041960a371c6b7a0342b4b214b0ff2d75cb9
Alexey <alexey.bearded@gmail.com>
2012-04-11 03:13:06 +0300

    Added --parent option into ModelGenerator

--- lib/rails/generators/mongo_mapper/model/templates/model.rb
+++ lib/rails/generators/mongo_mapper/model/templates/model.rb
@@ -1,13 +1,17 @@
-<% module_namespacing do -%>
+<%- module_namespacing do -%>
+<%- if parent_class_name.present? -%>
+class <%= class_name %> < <%= parent_class_name.classify %>
+<%- else -%>
 class <%= class_name %>
   include MongoMapper::Document
+<%- end -%>
 
-<% attributes.each do |attribute| -%>
+<%- attributes.each do |attribute| -%>
   key :<%= attribute.name %>, <%= attribute.type.to_s.camelcase %>
 <% end -%>
-<% if options[:timestamps] -%>
+<%- if options[:timestamps] -%>
   timestamps!
-<% end -%>
+<%- end -%>
 
 end
-<% end -%>
+<%- end -%>
