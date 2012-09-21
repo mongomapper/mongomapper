b12af57cb4ec63ce10f74f4764b46590f05a1bdb
John Nunemaker <nunemaker@gmail.com>
2012-09-21 17:39:31 -0400

    Use a bson ordered hash to fix 1.8.7/ree.

--- test/functional/test_modifiers.rb
+++ test/functional/test_modifiers.rb
@@ -2,8 +2,9 @@ require 'test_helper'
 
 class ModifierTest < Test::Unit::TestCase
   def setup
+    compound_key = BSON::OrderedHash['n', 42, 'i', BSON::ObjectId.new]
     @page_class_with_compound_key = Doc do
-      key :_id,                  :default => lambda { {:n => 42, :i => BSON::ObjectId.new} }
+      key :_id,                  :default => lambda { compound_key }
       key :title,       String
       key :day_count,   Integer, :default => 0
       key :week_count,  Integer, :default => 0
