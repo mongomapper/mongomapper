65b04fd57355b0f3a2ec9e01800b0067adc65c0c
John Nunemaker <nunemaker@gmail.com>
2012-09-21 11:50:37 -0400

    Minor: formatting

--- lib/mongo_mapper/connection.rb
+++ lib/mongo_mapper/connection.rb
@@ -65,7 +65,7 @@ module MongoMapper
       raise 'Set config before connecting. MongoMapper.config = {...}' if config.blank?
       env = config_for_environment(environment)
 
-      if env['options'].is_a? Hash
+      if env['options'].is_a?(Hash)
         options = env['options'].symbolize_keys.merge(options)
       end
       
