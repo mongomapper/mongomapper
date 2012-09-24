a70195fe914d21b4ae36002bc2a29adbc2003537
Leif Ringstad <leifcr@gmail.com>
2012-09-24 14:04:43 +0200

    Timefix for both 1.9.3 and REE using iso8601 instead of round.

--- lib/mongo_mapper/extensions/time.rb
+++ lib/mongo_mapper/extensions/time.rb
@@ -8,12 +8,7 @@ module MongoMapper
         else
           time_class = ::Time.try(:zone).present? ? ::Time.zone : ::Time
           time = value.is_a?(::Time) ? value : time_class.parse(value.to_s)
-          # for ree (1.8.7) versions
-          if RUBY_VERSION < "1.9"
-            at(Float((time.to_f*1000).to_i)/Float(1000)).utc if time # ensure milliseconds are preserved with to_f (issue #308)            
-          else
-            at(time.to_f).utc.round(3) if time # ensure milliseconds are preserved with to_f (issue #308)            
-          end
+          iso8601(time.utc.iso8601(3)).utc if time # keep time with milliseconds. (Same as BSON time)
         end
       end
 
