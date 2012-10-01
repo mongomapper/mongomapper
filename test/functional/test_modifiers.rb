mongomapper/mongomapper

commit cf82ec6e7d4874dd4860d433a995ea2e5add9cdc

there's no need to test for args length in criteria_and_keys_from_args

lib/mongo_mapper/plugins/modifiers.rb
@@ -73,21 +73,15 @@ module MongoMapper
           end
 
           def criteria_and_keys_from_args(args)
-            if args.length < 3
-              options = args[2].nil? ? nil : args[2]
-              updates = args[1].nil? ? nil : args[1]
-              criteria = args[0].is_a?(Hash) ? args[0] : {:id => args}
+            if args[0].is_a?(Hash)
+              criteria = args[0]
+              updates = args[1]
+              options = args[2]
             else
-              if args[0].is_a?(Hash)
-                criteria = args[0].nil? ? nil : args[0]
-                updates = args[1].nil? ? nil : args[1]
-                options = args[2].nil? ? nil : args[2]
-              else
-                split_args = args.partition{|a| a.is_a?(BSON::ObjectId)}
-                criteria = {:id => split_args[0]}
-                updates = split_args[1].first
-                options = split_args[1].last
-              end    
+              split_args = args.partition{|a| a.is_a?(BSON::ObjectId)}
+              criteria = {:id => split_args[0]}
+              updates = split_args[1].first
+              options = split_args[1].last
             end    
             [criteria_hash(criteria).to_hash, updates, options]
           end

test/functional/test_modifiers.rb
@@ -1,5 +1,5 @@
-# require 'test_helper'
-require File.expand_path("../../test_helper", __FILE__)
+require 'test_helper'
+
 class ModifierTest < Test::Unit::TestCase
   def setup
     @page_class = Doc do
@@ -329,7 +329,7 @@ class ModifierTest < Test::Unit::TestCase
         new_key_value = DateTime.now.to_s
         @page_class.increment({:title => new_key_value}, {:day_count => 1}, {:upsert => true})
         @page_class.count(:title => new_key_value).should == 1
-        # @page_class.first(:title => new_key_value).day_count.should == 1
+        @page_class.first(:title => new_key_value).day_count.should == 1
       end
       
       should "be able to pass safe option" do

commit 73f30d961cf03a11d6c4dec0ee454ac0292b44c3

Support non-ObjectID ids being given to modifiers.
The partially broken by cf82ec6e where it allowed a single ID to be
passed and it would work as expected. This also adds support for
multiple IDs to be provided.

lib/mongo_mapper/plugins/modifiers.rb
@@ -77,14 +77,13 @@ module MongoMapper
           def criteria_and_keys_from_args(args)
             if args[0].is_a?(Hash)
               criteria = args[0]
-              updates = args[1]
-              options = args[2]
+              updates  = args[1]
+              options  = args[2]
             else
-              split_args = args.partition{|a| a.is_a?(BSON::ObjectId)}
-              criteria = {:id => split_args[0]}
-              updates = split_args[1].first
-              options = split_args[1].last
+              criteria, (updates, options) = args.partition { |a| !a.is_a?(Hash) }
+              criteria = { :id => criteria }
             end
+
             [criteria_hash(criteria).to_hash, updates, options]
           end
       end

test/functional/test_modifiers.rb
@@ -109,6 +109,13 @@ class ModifierTest < Test::Unit::TestCase
         assert_page_counts @page, 1, 2, 3
         assert_page_counts @page2, 1, 2, 3
       end
+
+      should "work with ids given as strings" do
+        @page_class.increment(@page.id.to_s, @page2.id.to_s, :day_count => 1, :week_count => 2, :month_count => 3)
+
+        assert_page_counts @page, 1, 2, 3
+        assert_page_counts @page2, 1, 2, 3
+      end
     end
 
     context "decrement" do
@@ -133,6 +140,13 @@ class ModifierTest < Test::Unit::TestCase
 
       should "decrement with positive or negative numbers" do
         @page_class.decrement(@page.id, @page2.id, :day_count => -1, :week_count => 2, :month_count => -3)
+
+        assert_page_counts @page, 0, 0, 0
+        assert_page_counts @page2, 0, 0, 0
+      end
+
+      should "work with ids given as strings" do
+        @page_class.decrement(@page.id.to_s, @page2.id.to_s, :day_count => -1, :week_count => 2, :month_count => -3)
 
         assert_page_counts @page, 0, 0, 0
         assert_page_counts @page2, 0, 0, 0