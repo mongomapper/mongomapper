7a653a8066e3d3e3d25148aee37a5997a801f91d
Chris Heald <cheald@gmail.com>
2012-12-07 05:07:36 -0700

    Instead of forcing safety on by default, let the connection settings specify it. Add w, pool_size, and ssl option defaults to the config.

--- lib/mongo_mapper/plugins/modifiers.rb
+++ lib/mongo_mapper/plugins/modifiers.rb
@@ -79,19 +79,19 @@ module MongoMapper
               criteria = args[0]
               updates  = args[1]
               options  = args[2]
-              upgrade_legacy_safe_usage(options)
             else
               criteria, (updates, options) = args.partition { |a| !a.is_a?(Hash) }
               criteria = { :id => criteria }
             end
+            upgrade_legacy_safe_usage!(options)
 
             [criteria_hash(criteria).to_hash, updates, options]
           end
 
-          def upgrade_legacy_safe_usage(options)
+          def upgrade_legacy_safe_usage!(options)
             if options and options.key?(:safe)
-              options.delete :safe
               options.merge! Utils.get_safe_options(options)
+              options.delete :safe
             end
           end
       end
--- lib/mongo_mapper/utils.rb
+++ lib/mongo_mapper/utils.rb
@@ -1,8 +1,9 @@
 module MongoMapper
   module Utils
     def self.get_safe_options(options)
+      return {} unless options and options.key? :safe
       safe = options[:safe]
-      safe = {:w => 1} if safe == true or safe.nil?
+      safe = {:w => 1} if safe == true
       safe = {:w => 0} if safe == false
       safe = {:w => safe} if safe.is_a? Fixnum
       safe
