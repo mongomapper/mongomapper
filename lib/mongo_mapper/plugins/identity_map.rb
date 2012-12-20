2fd982e51b1504b646824fa0972c812bbfa369cf
Jonathan Chambers <j.chambers@gmx.net>
2012-12-20 22:16:09 +0000

    support for avoiding more complex queries if we can use the IM

--- lib/mongo_mapper/plugins/identity_map.rb
+++ lib/mongo_mapper/plugins/identity_map.rb
@@ -54,18 +54,49 @@ module MongoMapper
       module ClassMethods
         # Private - Looks for a document in the identity map
         def get_from_identity_map(id)
-          IdentityMap.repository[IdentityMap.key(self, id)]
+          IdentityMap.repository[IdentityMap.key(self, id)].tap do |doc|
+            if MongoMapper.logger and doc
+              MongoMapper.logger.info "MONGODB IDENTITY MAP HIT FOR #{id}" if IdentityMap.enabled? and MongoMapper.logger
+            else
+              MongoMapper.logger.info "MONGODB IDENTITY MAP MISS #{id} :-(" if IdentityMap.enabled? and MongoMapper.logger
+            end
+          end
         end
 
         module IdentityMapQueryMethods
+          
+          # def all(opts={})
+            # MongoMapper.logger.info ">>> in all"
+            # query = clone.amend(opts)
+            # super.tap do |docs|
+              # model.remove_documents_from_map(docs) if query.fields?
+            # end
+          # end
+          
           def all(opts={})
+            MongoMapper.logger.info ">>> in all" if MongoMapper.logger
             query = clone.amend(opts)
-            super.tap do |docs|
-              model.remove_documents_from_map(docs) if query.fields?
+            [].tap do |docs|
+              if IdentityMap.enabled? && query.simple?
+                if query[:_id].is_a?(Hash) && ids = query[:_id]["$in"]
+                  if query[:sort].nil?
+                    ids.each do |id|
+                      if doc = model.get_from_identity_map(id)
+                        docs << doc 
+                        ids.delete id
+                      end
+                    end
+                    break docs if ids.empty?
+                  end
+                else break [find_one(opts)].compact
+                end
+              end
+              find_each(query.criteria) { |doc| docs << doc }
             end
           end
 
           def find_one(opts={})
+            MongoMapper.logger.info ">>> in find_one" if MongoMapper.logger
             query = clone.amend(opts)
 
             if IdentityMap.enabled? && query.simple? && (document = model.get_from_identity_map(query[:_id]))
@@ -76,7 +107,7 @@ module MongoMapper
               end
             end
           end
-
+          
           def find_each(opts={}, &block)
             query = clone.amend(opts)
             super(opts) do |doc|
@@ -84,9 +115,14 @@ module MongoMapper
               block.call(doc) unless block.nil?
             end
           end
+          
+          
         end
+        
+        
 
         def query(opts={})
+          MongoMapper.logger.info ">>> in query" if MongoMapper.logger
           super.extend(IdentityMapQueryMethods)
         end
 
@@ -97,6 +133,10 @@ module MongoMapper
         end
 
         def load(attrs)
+          if MongoMapper.logger
+            MongoMapper.logger.info ">>> in load"
+            MongoMapper.logger.info ">>> but leaving because attrs.nil?" if attrs.nil?
+          end
           return nil if attrs.nil?
           document = get_from_identity_map(attrs['_id'])
 
@@ -119,6 +159,7 @@ module MongoMapper
 
       def add_to_identity_map
         if IdentityMap.enabled?
+          MongoMapper.logger.info ">>> adding in add_to_identity_map" if MongoMapper.logger
           key = IdentityMap.key(self.class, _id)
           IdentityMap.repository[key] = self
         end
@@ -126,6 +167,7 @@ module MongoMapper
 
       def remove_from_identity_map
         if IdentityMap.enabled?
+          MongoMapper.logger.info ">>> removing in remove_from_identity_map" if MongoMapper.logger
           key = IdentityMap.key(self.class, _id)
           IdentityMap.repository.delete(key)
         end
