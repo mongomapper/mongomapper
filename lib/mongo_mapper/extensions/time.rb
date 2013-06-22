54e2b0a67967d43ea554579d2a1e5e271bee729c
Chris Heald <cheald@gmail.com>
2013-06-22 11:51:03 -0700

    Since MM depends on activesupport already, just go ahead and include the Time.zone patch so we can remove #try

--- lib/mongo_mapper/extensions/time.rb
+++ lib/mongo_mapper/extensions/time.rb
@@ -1,3 +1,5 @@
+require 'active_support/core_ext/time/zones'
+
 # encoding: UTF-8
 module MongoMapper
   module Extensions
@@ -6,14 +8,14 @@ module MongoMapper
         if !value || '' == value
           nil
         else
-          time_class = ::Time.try(:zone) || ::Time
+          time_class = ::Time.zone || ::Time
           time = value.is_a?(::Time) ? value : time_class.parse(value.to_s)
           at(time.to_f).utc if time # ensure milliseconds are preserved with to_f (issue #308)
         end
       end
 
       def from_mongo(value)
-        if value and zone = ::Time.try(:zone)
+        if value and zone = ::Time.zone
           value.in_time_zone(zone)
         else
           value
