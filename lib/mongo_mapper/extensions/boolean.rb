f7ad3ef0851b8346b3700250b409793234035c60
Chris Heald <cheald@gmail.com>
2013-06-24 09:36:46 -0700

    Performance improvements to boolean#from_mongo

--- lib/mongo_mapper/extensions/boolean.rb
+++ lib/mongo_mapper/extensions/boolean.rb
@@ -3,24 +3,24 @@ module MongoMapper
   module Extensions
     module Boolean
       Mapping = {
-        true    => true, 
-        'true'  => true, 
-        'TRUE'  => true, 
-        'True'  => true, 
-        't'     => true, 
-        'T'     => true, 
-        '1'     => true, 
-        1       => true, 
+        true    => true,
+        'true'  => true,
+        'TRUE'  => true,
+        'True'  => true,
+        't'     => true,
+        'T'     => true,
+        '1'     => true,
+        1       => true,
         1.0     => true,
-        false   => false, 
-        'false' => false, 
-        'FALSE' => false, 
-        'False' => false, 
-        'f'     => false, 
-        'F'     => false, 
-        '0'     => false, 
-        0       => false, 
-        0.0     => false, 
+        false   => false,
+        'false' => false,
+        'FALSE' => false,
+        'False' => false,
+        'f'     => false,
+        'F'     => false,
+        '0'     => false,
+        0       => false,
+        0.0     => false,
         nil     => nil
       }
 
@@ -33,7 +33,8 @@ module MongoMapper
       end
 
       def from_mongo(value)
-        value.nil? ? nil : !!value
+        return nil if value == nil
+        !!value
       end
     end
   end
