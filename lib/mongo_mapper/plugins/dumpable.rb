47b9a520ab9975feea5f84dd7970f8aa635159bf
Chris Heald <cheald@gmail.com>
2013-07-05 14:42:20 -0700

    Fix problems with @errors not being dumpable on 1.8

--- lib/mongo_mapper/plugins/dumpable.rb
+++ lib/mongo_mapper/plugins/dumpable.rb
@@ -3,10 +3,10 @@ require 'set'
 module MongoMapper
   module Plugins
     module Dumpable
-      DUMP_BLACKLIST = Set.new([:@_mm_default_keys, :@_mm_keys])
+      DUMP_BLACKLIST = Set.new([:@_mm_default_keys, :@_mm_keys, :@errors])
 
       def marshal_dump
-        instance_variables.inject({}) do |h, var|
+        instance_variables.map(&:to_sym).inject({}) do |h, var|
           h[var] = instance_variable_get(var) unless DUMP_BLACKLIST.include?(var)
           h
         end
