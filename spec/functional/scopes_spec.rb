mongomapper/mongomapper

commit 377754dbcdf9c877cc1680b50b3d5b8c33ff80d0

bug fix: make sure that scopes get applied onto DecoratedPluckyQuery, not just on has many proxies

lib/mongo_mapper/plugins/querying/decorated_plucky_query.rb
@@ -63,11 +63,8 @@ module MongoMapper
         def method_missing(method, *args, &block)
           return super unless model.respond_to?(method)
 
-          result = model.send(method, *args, &block)
-          if result.is_a?(Plucky::Query)
-            merge(result)
-          else
-            result
+          model.with_scope(criteria_hash) do
+            model.send(method, *args, &block)
           end
         end
       end

spec/functional/associations/many_documents_proxy_spec.rb
@@ -960,6 +960,7 @@ describe "ManyDocumentsProxy" do
       @training_class = Doc do
         set_collection_name "trainings"
         key :slug, String
+        key :is_active, Boolean, default: true
 
         def self.find_by_slug!(the_slug)
           if res = first(:slug => the_slug)
@@ -993,6 +994,27 @@ describe "ManyDocumentsProxy" do
 
       @job_title_1.trainings.find_by_slug!('foo').should == @training_1
       @job_title_2.trainings.find_by_slug!('foo').should == @training_2
+
+      lambda do
+        @job_title_2.trainings.find_by_slug!('bar')
+      end.should raise_error
+    end
+
+    it "should scope with an extra where clause on the proxy (regression #2)" do
+      @job_title_1 = @job_title_class.create!
+      @job_title_2 = @job_title_class.create!
+
+      @training_1 = @training_class.create!(:slug => 'foo', :job_title_id => @job_title_1.id)
+      @training_2 = @training_class.create!(:slug => 'foo', :job_title_id => @job_title_2.id)
+
+      @job_title_1.reload
+      @job_title_2.reload
+
+      @job_title_1.trainings.count.should == 1
+      @job_title_2.trainings.count.should == 1
+
+      @job_title_1.trainings.where(:is_active => true).find_by_slug!('foo').should == @training_1
+      @job_title_2.trainings.where(:is_active => true).find_by_slug!('foo').should == @training_2
 
       lambda do
         @job_title_2.trainings.find_by_slug!('bar')

commit bf08ac8caf4cc5964037fcad779d99fbb1756392

Add spec (and fix bug introduced in 377754dbcdf9c877cc1680b50b3d5b8c33ff80d0) regarding merging Plucky::Queries in DecoratedPluckyQuery's method_missing

lib/mongo_mapper/plugins/querying/decorated_plucky_query.rb
@@ -63,8 +63,15 @@ module MongoMapper
         def method_missing(method, *args, &block)
           return super unless model.respond_to?(method)
 
-          model.with_scope(criteria_hash) do
+          result = model.with_scope(criteria_hash) do
             model.send(method, *args, &block)
+          end
+
+          case result
+          when Plucky::Query
+            merge(result)
+          else
+            result
           end
         end
       end

spec/functional/scopes_spec.rb
@@ -262,6 +262,17 @@ describe "Scopes" do
         @klass.all.should include(u2)
       end
 
+      it "should return the value of the block" do
+        u1 = @klass.create!(:first_name => "Scott")
+        u2 = @klass.create!(:first_name => "Andrew")
+
+        result = @klass.with_scope(:first_name => "Scott") do
+          @klass.all
+        end
+
+        result.should == [u1]
+      end
+
       it "should have an empty list of default scope" do
         @klass.default_scopes.should == []
       end
@@ -344,6 +355,25 @@ describe "Scopes" do
         @subclass.default_scopes.length.should == 2
         @klass.default_scopes.length.should == 1
       end
+    end
+  end
+
+  describe "regression - multiple named scopes" do
+    it "should merge the hashes from multiple scopes" do
+      klass = Doc do
+        key :a, Boolean
+        key :b, Boolean
+        key :c, Boolean
+
+        scope :one, where(:a => true, :b => true)
+        scope :two, where(:c => true)
+      end
+
+      obj = klass.create!(:a => false, :b => true, :c => true)
+      klass.one.two.all.should == []
+
+      obj2 = klass.create(:a => true, :b => true, :c => true)
+      klass.one.two.all.should == [obj2]
     end
   end
 end