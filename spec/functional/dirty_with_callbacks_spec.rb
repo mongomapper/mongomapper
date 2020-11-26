ad9e8196d125d243ffe1351ed135b869417ba3df
ujihisa <ujihisa@users.noreply.github.com>
2020-11-25 19:41:00 -0800

    Test after_save
    * Also cover the case when some callbacks didn't get called

--- spec/functional/dirty_with_callbacks_spec.rb
+++ spec/functional/dirty_with_callbacks_spec.rb
@@ -2,21 +2,58 @@ require 'spec_helper'
 
 describe 'DirtyWithCallbacks' do
   it 'should update its changes/previous_changes before after_create/after_update callbacks' do
+    history = {}
+
     document = Doc {
       key :x, String
 
+      after_save {
+        history[:after_save] = {
+          changes: changes,
+          previous_changes: previous_changes,
+        }
+      }
       after_create {
-        changes.should == {}
-        previous_changes.should == {'x' => [nil, 'hello']}
+        history[:after_create] = {
+          changes: changes,
+          previous_changes: previous_changes,
+        }
       }
       after_update {
-        changes.should == {}
-        previous_changes.should == {'x' => ['hello', 'world']}
+        history[:after_update] = {
+          changes: changes,
+          previous_changes: previous_changes,
+        }
       }
     }
 
-    d = document.create(x: 'hello')
+    d = document.new(x: 'hello')
+    d.save
+
+    history.should == {
+      after_save: {
+        changes: {},
+        previous_changes: {'x' => [nil, 'hello']},
+      },
+      after_create: {
+        changes: {},
+        previous_changes: {'x' => [nil, 'hello']},
+      },
+    }
+    history.clear
+
     d.x = 'world'
     d.save!
+
+    history.should == {
+      after_save: {
+        changes: {},
+        previous_changes: {'x' => ['hello', 'world']},
+      },
+      after_update: {
+        changes: {},
+        previous_changes: {'x' => ['hello', 'world']},
+      },
+    }
   end
 end
