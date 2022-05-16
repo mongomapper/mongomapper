5da848f4a93fb77601a1975d804ecfa2c46b9f92
Masato Ikeda <masato.ikeda@gmail.com>
2022-05-16 22:03:17 +0900

    Run tests against MongoDB 4.2, 4.4, 5.0, and their sharded cluster

--- spec/functional/shardable_spec.rb
+++ spec/functional/shardable_spec.rb
@@ -1,23 +1,15 @@
 require 'spec_helper'
 
 describe 'Shardable' do
-  let(:sharded_model) {
-    Doc do
-      key :first_name, String
-      key :last_name, String
-      shard_key :first_name, :last_name
-    end
-  }
-
   describe 'shard_key_fields' do
     it 'returns declared field names' do
-      sharded_model.shard_key_fields.should == ['first_name', 'last_name']
+      ShardedModel.shard_key_fields.should == ['first_name', 'last_name']
     end
   end
 
   describe 'shard_key_filter' do
     context 'new record' do
-      let(:document) { sharded_model.new(first_name: 'John', last_name: 'Smith') }
+      let(:document) { ShardedModel.new(first_name: 'John', last_name: 'Smith') }
 
       it 'returns current values' do
         document.shard_key_filter.should == { 'first_name' => 'John', 'last_name' => 'Smith' }
@@ -25,7 +17,7 @@ describe 'Shardable' do
     end
 
     context 'persisted record' do
-      let(:document) { sharded_model.create!(first_name: 'John', last_name: 'Smith') }
+      let(:document) { ShardedModel.create!(first_name: 'John', last_name: 'Smith') }
 
       before do
         document.first_name = 'William'
@@ -38,28 +30,28 @@ describe 'Shardable' do
   end
 
   context 'creating new document' do
-    let(:document) { sharded_model.new(first_name: 'John', last_name: 'Smith') }
+    let(:document) { ShardedModel.new(first_name: 'John', last_name: 'Smith') }
 
     it 'inserts new document' do
-      lambda { document.save! }.should change { sharded_model.count }.by(1)
+      lambda { document.save! }.should change { ShardedModel.count }.by(1)
 
-      persisted = sharded_model.find(document.id)
+      persisted = ShardedModel.find(document.id)
       persisted.first_name.should == 'John'
       persisted.last_name.should == 'Smith'
     end
   end
 
   context 'updating persisted document' do
-    let(:document) { sharded_model.create!(first_name: 'John', last_name: 'Smith') }
+    let(:document) { ShardedModel.create!(first_name: 'John', last_name: 'Smith') }
 
     before do
       document.first_name = 'William'
     end
 
     it 'updates persisted document' do
-      lambda { document.save! }.should_not change { sharded_model.count }
+      lambda { document.save! }.should_not change { ShardedModel.count }
 
-      persisted = sharded_model.find(document.id)
+      persisted = ShardedModel.find(document.id)
       persisted.first_name.should == 'William'
       persisted.last_name.should == 'Smith'
     end
---
+++ spec/support/sharded_model.rb
@@ -0,0 +1,26 @@
+class ShardedModel
+  include MongoMapper::Document
+
+  key :first_name, String
+  key :last_name, String
+  shard_key :first_name, :last_name
+end
+
+if ENV.fetch("ENABLE_SHARDING", "0") == "1"
+  client = MongoMapper.connection
+  database = MongoMapper.database
+
+  # https://www.mongodb.com/docs/manual/reference/command/enableSharding/#mongodb-dbcommand-dbcmd.enableSharding
+  client.use(:admin).command(enableSharding: database.name)
+
+  # https://www.mongodb.com/docs/manual/reference/command/shardCollection/#mongodb-dbcommand-dbcmd.shardCollection
+  # Note 1: this command automatically creates the index for the empty collection
+  # Note 2: shard key can contain at most one 'hashed' field, and/or multiple numerical fields set to a value of 1
+  client.use(:admin).command(
+    shardCollection: [database.name, ShardedModel.collection.name].join("."),
+    key: {
+      first_name: "hashed",
+      last_name: 1,
+    },
+  )
+end
